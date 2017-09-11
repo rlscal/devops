b4e519239ea9fc20edda0ceb25b1bd05ab4d8ff4
rlscal <rlscal@gmail.com>
2017-09-11 17:55:40 -0300

    Update vsts_cordova.ps1

--- vsts_cordova.ps1
+++ vsts_cordova.ps1
@@ -15,8 +15,7 @@ if (-Not $Env:variables_status) {
 fn_get_vsts_variables;
 }
 fn_display_variables;
-if ($Env:BUILD_REPOSITORY_URI) {
-$Env:git_urlcred="https://$Env:git_user:$Env:git_pwd@$($Env:BUILD_REPOSITORY_URI.ToString().SubString(8))";
+if ($Env:git_urlcred) {
 echo "remote set-url origin `"$Env:git_urlcred`""; #git remote set-url origin "$Env:git_urlcred" 2>&1 ;
 echo "checkout master" ; # git checkout master 2>&1 ;
 echo "git show-branch --all"; # git show-branch --all;
@@ -25,11 +24,15 @@ echo "git config --global user.name `"$($Env:git_config_username)`""; # git conf
 echo "git config --global user.email `"$($Env:git_config_email)`""; # git config --global user.email "$($Env:git_config_email)";
 }
 else {
-echo "ERROR, missing or empty variable `$Env:BUILD_REPOSITORY_URI";
+echo "ERROR, missing or empty variable `$Env:git_urlcred";
 }
 }
 
 function global:fn_gitcommitpush([string]$commit_msg) {
+if (-Not $Env:variables_status) {
+fn_get_vsts_variables;
+fn_display_variables;
+}
 if ($Env:git_urlcred) {
 echo "git add -A"; # git add -A 2>&1;
 echo "git commit -m `"$commit_msg`""; # git commit -m "$commit_msg" 2>&1;
@@ -45,12 +48,19 @@ function global:fn_get_vsts_variables() {
 $Env:variables_status="OK";
 
 #$Env:BUILD_REPOSITORY_URI : come from VSTS Get Ressource Task;
+if ($Env:BUILD_REPOSITORY_URI) {
 $Env:CORDOVA_DEFAULT_VERSION=$(CORDOVA_DEFAULT_VERSION);
 $Env:appname=$(appname);
 $Env:git_config_email=$(git_config_email); # email for GIT commit under VSTS
 $Env:git_config_username=$(git_config_username); # user name for GIT commit under VSTS
 $Env:git_pwd=$(git_pwd); # user pwd for git remote access
 $Env:git_user=$(git_user); # user login for git remote access
+
+$Env:git_urlcred="https://$Env:git_user:$Env:git_pwd@$($Env:BUILD_REPOSITORY_URI.ToString().SubString(8))";
+}
+else {
+echo "ERROR, missing or empty variable `$Env:BUILD_REPOSITORY_URI";
+}
 }
 
 function global:fn_display_variables() {
@@ -61,5 +71,8 @@ echo "`$Env:git_config_email=$Env:git_config_email";
 echo "`$Env:git_config_username=$Env:git_config_username";
 echo "`$Env:git_pwd=$Env:git_pwd";
 echo "`$Env:git_user=$Env:git_user";
+
+echo "`$Env:git_urlcred=$Env:git_urlcred";
+
 }
 echo "End loading vsts_cordova.ps1"
