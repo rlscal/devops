c07dff05e6b9a9e7e569bad18f977d8db676b562
rlscal <rlscal@gmail.com>
2017-09-18 14:00:41 -0300

    under validation

--- vsts_cordova.js
+++ vsts_cordova.js
@@ -1,13 +1,49 @@
-
-// Usage under VSTS project in nodejs command line
-// 1) After 'Get Ressource' task for initialization (variable and git config):
+// 
+// Download this js module once per build pipelile (workflow)
 // (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/rlscal/devops/master/vsts_cordova.js", "$home\vsts_cordova.js");
-// node $home\vsts_cordova.js gitinit;
+// call with "--help" option to get information
 
-// 2) To commmit & push to remote GIT:
-// node $home\vsts_cordova.js  gitcommitpush "your comment"
+lc_scriptname="vsts_cordova.js";
+console.log("Start " + lc_scriptname);
 
-console.log("Start loading vsts_cordova.js");
+function fn_help() {
+    console.log("To be used in VSTS project in nodejs command line");
+
+    console.log("Usage: node $home/" + lc_scriptname + " command [option] ");
+    console.log("  Commands");
+    console.log("\t--help : \t\t\tDisplay module help");
+    
+    console.log("\t--gitinit : \t\t\t\
+For git initialization (git config), after After 'Get Ressource' task.\n");
+
+    console.log("\t--gitcommitpush {'your_comment'} : \t\
+To commmit & push to remote GIT with your commant in parameter.\n");
+}
+
+function fn_init() {
+    console.log(process.argv);
+    
+    if ((process.argv.length <= 2)) {
+        fn_help();
+         // process.exit(-1);
+    }
+    else
+    {
+        switch (process.argv[2])
+        {
+        case "--gitinit":
+            fn_gitinit();
+        break;
+        case "--gitcommitpush":
+            fn_gitcommitpush(process.argv[3]);
+        break;
+        default: 
+            fn_help();
+        break;
+        }
+    }
+}
+
 function fn_gitinit() {
 if (! process.env.variables_status) {
 fn_get_vsts_variables();
@@ -18,22 +54,29 @@ console.log("remote set-url origin \"$git_urlcred\""); // git remote set-url ori
 console.log("checkout master") ; // git checkout master 2>&1 ;
 console.log("git show-branch --all"); // git show-branch --all;
 console.log("git config --global core.autocrlf input"); // git config --global core.autocrlf input 2>&1;
-console.log("git config --global user.name \"$git_config_username\""); // git config --global user.name process.env.git_config_username;
-console.log("git config --global user.email \"$process.env.git_config_email\""); // git config --global user.email process.env.git_config_email;
+console.log("git config --global user.name " + process.env.git_config_username); // git config --global user.name process.env.git_config_username;
+console.log("git config --global user.email " + process.env.git_config_email); // git config --global user.email process.env.git_config_email;
 }
 else {
 console.log("ERROR, missing or empty variable $git_urlcred");
 }
 }
 
-function fn_gitcommitpush([string]$commit_msg) {
+function fn_gitcommitpush(commit_msg) {
+if (!commit_msg) {
+    commit_msg = "";
+}
+else if (!(typeof commit_msg !== 'string') ) {
+    commit_msg = commit_msg.toString();
+}
+
 if (! process.env.variables_status) {
 fn_get_vsts_variables();
 fn_display_variables();
 }
 if (process.env.git_urlcred) {
 console.log("git add -A"); // git add -A 2>&1;
-console.log("git commit -m \"$commit_msg\""); // git commit -m "$commit_msg" 2>&1;
+console.log("git commit -m \"" + commit_msg + "\""); // git commit -m "$commit_msg" 2>&1;
 console.log("git push origin master"); // git push origin master 2>&1;
 }
 else {
@@ -54,7 +97,7 @@ if (process.env.BUILD_REPOSITORY_URI) {
 //process.env.git_pwd=; // user pwd for git remote access
 //process.env.git_user=; // user login for git remote access
 
-process.env.git_urlcred="https://"+process.env.git_user+":"+process.env.git_pwd+"@"+process.env.BUILD_REPOSITORY_URI.toString().subString(8));
+process.env.git_urlcred="https://"+process.env.git_user+":"+process.env.git_pwd+"@"+process.env.BUILD_REPOSITORY_URI.toString().substring(8);
 }
 else {
 console.log("ERROR, missing or empty variable $BUILD_REPOSITORY_URI");
@@ -73,6 +116,9 @@ console.log("$git_user="+process.env.git_user);
 console.log("$git_urlcred="+process.env.git_urlcred);
 
 }
-console.log("End loading vsts_cordova.js")
+
+fn_init();
+
+console.log("End " + lc_scriptname)
 
 
